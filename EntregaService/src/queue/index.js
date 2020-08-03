1ef076c791477421ea9900508cf53ed7a20f8dfa
Caio Favero <caio.vf@gmail.com>
2020-08-03 11:00:56 -0300

    refactor: docker-compose to send external API info

---
+++ EntregaService/src/queue/index.js
@@ -0,0 +1,28 @@
+const q = 'tasks'
+const open = require('amqplib').connect('amqp://localhost')
+
+// Publisher
+open
+  .then(conn => conn.createChannel())
+  .then(ch => {
+    return ch.assertQueue(q)
+      .then(ok => {
+        return ch.sendToQueue(q, Buffer.from('something to do'))
+      })
+  })
+  .catch(console.warn)
+
+// Consumer
+open
+  .then(conn => conn.createChannel())
+  .then(ch => {
+    return ch.assertQueue(q).then(ok => {
+      return ch.consume(q, msg => {
+        if (msg !== null) {
+          console.log(msg.content.toString())
+          ch.ack(msg)
+        }
+      })
+    })
+  })
+  .catch(console.warn)
