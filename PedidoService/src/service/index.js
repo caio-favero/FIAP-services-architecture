a912c50fede762e67026e004e1b83bac5b4b867e
Caio Favero <caio.vf@gmail.com>
2020-08-03 23:06:59 -0300

    add: fixed get from external api

--- PedidoService/src/service/index.js
+++ PedidoService/src/service/index.js
@@ -3,15 +3,12 @@ const axios = require('axios')
 module.exports = {
 
   entregaService(uf) {
-    return axios.get(`http://${process.env.DB_HOST}:8002/consultarPrazoEntrega/${uf}`)
-      .then(res => {
-        console.log('res => ', res)
-        return res
-      })
+    axios.get(`http://${process.env.ENTREGA_HOST}:8000/entregas/consultarPrazoEntrega/${uf}`)
+      .then(res => res.data)
       .catch(err => {
         const error = new Error(err)
         error.code = 400
-        console.log('error => ', error)
+
         return error
       })
   }
